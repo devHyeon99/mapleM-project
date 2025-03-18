5cf832ff9eaa00d2f72b41e3542ad82beeacbf4b
ehh99 <jinung5@gmail.com>
2025-03-18 18:32:13 +0900

    Style: Sidebar 스타일 수정 - 기존 1:3:1 분할 하려고 중앙 화면 양측에 있던 Border 삭제

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -7,7 +7,7 @@ export const Sidebar = ({ position }: SidebarProps) => {
 
   return (
     <aside
-      className={`w-full p-4 hidden xl:block dark:bg-[#171717] ${borderClass} dark:border-[#3a3a3e]`}
+      className={`w-full p-4 hidden xl:block border-none ${borderClass}`}
     />
   );
 };

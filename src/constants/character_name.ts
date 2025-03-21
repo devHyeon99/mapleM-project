7082149e03c5d6657e17531107bcca7f8b688dfc
ehh99 <jinung5@gmail.com>
2025-03-21 13:23:12 +0900

    feat: 메이플스토리 직업별 캐릭터 이미지 매핑 기능 추가
    - import.meta.glob으로 jobs 디렉토리 내 PNG 이미지 일괄 import
    - 직업 이름과 파일명을 매핑하는 jobNameToFileName 객체 정의
    - characterImageMap 객체를 생성하여 직업명에 해당하는 이미지 경로를 할당
    - 캐릭터 정보 api를 불러올시 직업에 맞는 일러스트를 보여주기 위함

---
+++ src/constants/character_name.ts
@@ -0,0 +1,61 @@
+const images = import.meta.glob('@/assets/images/jobs/*.png', { eager: true });
+
+const jobNameToFileName: Record<string, string> = {
+  아델: 'adel',
+  엔젤릭버스터: 'angelicburster',
+  아란: 'aran',
+  아크: 'ark',
+  배틀메이지: 'battlemage',
+  비숍: 'bishop',
+  블래스터: 'blaster',
+  보우마스터: 'bowmaster',
+  '아크메이지(불,독)': 'bulldog',
+  캐논슈터: 'canon',
+  캡틴: 'captin',
+  데몬어벤져: 'daemonavenger',
+  다크나이트: 'darkknight',
+  데몬슬레이어: 'demonslayer',
+  듀얼블레이드: 'dualblade',
+  에릴: 'erel',
+  에반: 'evan',
+  플레임위자드: 'flamewizard',
+  히어로: 'hero',
+  호영: 'hoyoung',
+  카데나: 'kadena',
+  카인: 'kain',
+  카이저: 'kaiser',
+  키네시스: 'kinesis',
+  나이트워커: 'knightwalker',
+  라라: 'lara',
+  루미너스: 'luminous',
+  메카닉: 'mechanic',
+  메르세데스: 'mercedes',
+  미하일: 'mihile',
+  나이트로드: 'nightroad',
+  팔라딘: 'paladin',
+  패스파인더: 'pathfinder',
+  팬텀: 'phantom',
+  섀도어: 'shadow',
+  신궁: 'shrine',
+  시아: 'sia',
+  은월: 'silverwall',
+  소울마스터: 'soulmaster',
+  스트라이커: 'striker',
+  '아크메이지(썬,콜)': 'suncall',
+  바이퍼: 'viper',
+  와일드헌터: 'wildhunter',
+  윈드브레이커: 'windbreaker',
+  제논: 'xenon',
+};
+
+export const characterImageMap: Record<string, string> = {};
+
+for (const [jobName, fileName] of Object.entries(jobNameToFileName)) {
+  const path = `/src/assets/images/jobs/${fileName}.png`;
+  const imageModule = images[path] as { default: string } | undefined;
+  if (imageModule) {
+    characterImageMap[jobName] = imageModule.default;
+  } else {
+    console.warn(`이미지 파일이 없습니다: ${path}`);
+  }
+}

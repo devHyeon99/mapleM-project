1c026ba4d234f8b7869bf87a30c96403dc5d198a
ehh99 <jinung5@gmail.com>
2025-03-24 20:08:41 +0900

    feat: 계정 삭제용 AccountAlertDialog 컴포넌트 추가
    - 계정 삭제 여부를 확인하는 AlertDialog 컴포넌트 구현
    - 삭제 결과에 따라 toast 메시지 출력
    - AccountCardMenu에서 삭제 메뉴와 연결되어 사용됨
    - home 도메인 컴포넌트 엔트리 파일 추가

---
+++ src/features/home/components/index.ts
@@ -0,0 +1,4 @@
+export { AccountCard } from './AccountCard';
+export { AccountList } from './AccountList';
+export { AccountDialog } from './AccountDialog';
+export { default as AccountCardMenu } from './AccountCardMenu';

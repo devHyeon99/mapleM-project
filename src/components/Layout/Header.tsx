5e5792feb3e84877de85631cc773b009e3f81a18
ehh99 <jinung5@gmail.com>
2025-03-25 15:25:11 +0900

    refactor: Header 컴포넌트 UI 수정 - HeaderMenuGroup 컴포넌트 추가 - 모바일, 데스크톱 화면 기준 UI 렌더링 구현

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,36 +1,85 @@
-import { ChevronLeft } from 'lucide-react';
-import { ModeToggle } from '../mode-toggle';
+import { ChevronLeft, Menu } from 'lucide-react';
 import { useNavigate } from 'react-router';
 import { Button } from '@/components/ui/button';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { HeaderMenuGroup } from './HeaderMenuGroup';
 
 interface HeaderProps {
   title: string;
   showBackButton?: boolean;
+  showMenuBar?: boolean;
+  showCharacterHelpButton?: boolean;
 }
 
-export const Header = ({ title, showBackButton = false }: HeaderProps) => {
+export const Header = ({
+  title,
+  showBackButton = false,
+  showMenuBar = false,
+}: HeaderProps) => {
   const navigate = useNavigate();
 
-  const handleBack = () => {
-    navigate(-1);
-  };
+  const handleBack = () => navigate(-1);
 
   return (
-    <header className='bg-background flex justify-between items-center pb-6'>
+    <header className='bg-background flex items-center justify-between pb-6'>
       {showBackButton && (
         <Button
-          className='[&_svg]:!size-5'
           onClick={handleBack}
           size='icon'
           aria-label='뒤로 가기'
+          className='[&_svg]:!size-5 focus-visible:ring-4 focus-visible:ring-[#FFCC88] focus-visible:outline-none'
         >
           <ChevronLeft />
         </Button>
       )}
-      <h1 className='text-xl text-foreground flex items-center h-full font-semibold'>
+
+      <h1
+        className={`flex-1 flex items-center h-full text-xl text-foreground font-bold ${
+          showBackButton ? 'justify-center' : 'justify-start'
+        }`}
+      >
         {title}
       </h1>
-      <ModeToggle />
+
+      {/* 데스크탑 메뉴 */}
+      {showMenuBar && (
+        <nav className='hidden md:flex' aria-label='메인 메뉴'>
+          <HeaderMenuGroup />
+        </nav>
+      )}
+
+      {/* 모바일 메뉴 (햄버거) */}
+      {showMenuBar && (
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              size='icon'
+              variant='ghost'
+              className='md:hidden hover:bg-transparent'
+              tooltip='메뉴'
+              aria-label='메인 메뉴'
+            >
+              <Menu className='size-6' aria-hidden />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side='right'>
+            <SheetHeader>
+              <SheetTitle>Menu</SheetTitle>
+              <SheetDescription>MMGG Main Menu</SheetDescription>
+            </SheetHeader>
+            <nav className='flex flex-col' aria-label='메인 메뉴'>
+              <HeaderMenuGroup />
+            </nav>
+          </SheetContent>
+        </Sheet>
+      )}
     </header>
   );
 };

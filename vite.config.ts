f1b5e133bd42a4d9824169aa35d1f0331830c3ab
ehh99 <jinung5@gmail.com>
2025-03-31 16:13:53 +0900

    chore: 이미지 최적화 플러그인 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
-import path from 'path';
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    ViteImageOptimizer({
+      webp: {
+        lossless: true,
+        quality: 80,
+      },
+      logStats: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),

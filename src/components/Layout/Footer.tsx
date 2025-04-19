c81b72d3bd98b3cbdb29b0a7426909d22f5329d7
ehh99 <jinung5@gmail.com>
2025-04-19 11:21:53 +0900

    chore: footer 문구 수정 및 저작권 고지 추가
    - footer에 '© 2025 MMGG. 엘리시움 자유' 및 고지 문구 추가
    - 'This is an unofficial site and is not affiliated with NEXON Korea' 명시
    - Nexon Open API 이용약관에 따른 표기 준수에 따라 'Data based on NEXON Open API' 문구 추가

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,7 +1,12 @@
 export const Footer = () => {
   return (
-    <footer className='flex justify-center items-end text-foreground pt-6 bg-background'>
-      <span className='text-sm font-light'>엘리시움 자유 제작</span>
+    <footer className='flex justify-center items-center text-center text-foreground pt-6 bg-background'>
+      <p className='text-xs font-light'>
+        © 2025 MMGG. 엘리시움 자유
+        <br />
+        This is an unofficial site and is not affiliated with NEXON Korea. Data
+        based on NEXON Open API.
+      </p>
     </footer>
   );
 };

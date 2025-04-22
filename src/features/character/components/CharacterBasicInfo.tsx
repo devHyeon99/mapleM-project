bb890ed3a6f03c91e3e39a91d275785bf74a9d63
ehh99 <jinung5@gmail.com>
2025-04-22 17:38:51 +0900

    refactor: 캐릭터 생성일 렌더링 분기처리 및 UI 수정 - 캐릭터 생성일 데이터가 null일 경우 알 수 없음으로 표기 - 캐릭터 생성일 기존 날짜만 나오던 부분을 현재 날짜와 계산을 해서 표기 ex) 1321일전 - 마지막 로그인, 로그아웃, 동기화 글자 끝이 맞게 스타일 수정

--- src/features/character/components/CharacterBasicInfo.tsx
+++ src/features/character/components/CharacterBasicInfo.tsx
@@ -1,4 +1,5 @@
 import { CharacterData } from '@/types/character';
+import { getDaysSince } from '@/utils/dateUtils';
 
 interface BasicInfoProps {
   character: CharacterData;
@@ -8,12 +9,15 @@ export function CharacterBasicInfo({ character }: BasicInfoProps) {
   const basic = character.basic;
 
   return (
-    <section aria-labelledby='basic-info-title' className='flex-1 space-y-2'>
+    <section
+      aria-labelledby='basic-info-title'
+      className='flex flex-col flex-1 gap-5 p-2'
+    >
       <h3 id='basic-info-title' className='text-xl font-bold tracking-[0.2rem]'>
         {basic.character_name}
       </h3>
 
-      <dl className='space-y-3'>
+      <dl className='flex flex-col flex-1  gap-5'>
         <div className='flex gap-2'>
           <dt className='font-medium w-25'>서버</dt>
           <dd>{basic.world_name}</dd>
@@ -41,11 +45,19 @@ export function CharacterBasicInfo({ character }: BasicInfoProps) {
 
         <div className='flex gap-2'>
           <dt className='font-medium w-25'>생성일</dt>
-          <dd>{new Date(basic.character_date_create).toLocaleDateString()}</dd>
+          <dd>
+            {basic.character_date_create === null
+              ? '알 수 없음'
+              : `${new Date(
+                  basic.character_date_create
+                ).toLocaleDateString()} (${getDaysSince(
+                  basic.character_date_create
+                )}일 전)`}
+          </dd>
         </div>
 
         <div className='flex gap-2'>
-          <dt className='font-medium w-25'>마지막 로그인</dt>
+          <dt className='font-medium w-25 tracking-[0.128em]'>마지막 로그인</dt>
           <dd>
             {basic.character_date_last_login
               ? new Date(basic.character_date_last_login).toLocaleString()
@@ -63,7 +75,7 @@ export function CharacterBasicInfo({ character }: BasicInfoProps) {
         </div>
 
         <div className='flex gap-2'>
-          <dt className='font-medium w-25'>데이터 동기화</dt>
+          <dt className='font-medium w-25 tracking-[0.128em]'>데이터 동기화</dt>
           <dd>
             {character.lastUpdatedAt
               ? new Date(character.lastUpdatedAt).toLocaleString()

2c2aa97c8c67ca668ad10e4fad5b4881b2ed5780
ehh99 <jinung5@gmail.com>
2025-06-12 21:33:42 +0900

    feat: 캐릭터 아이템 스켈레톤 UI추가

---
+++ src/components/domain/scheduler/ScheduleDetails/ChecklistItemSkeleton.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Skeleton } from "@/components/ui/skeleton";
+
+export const ChecklistItemSkeleton = () => {
+  return (
+    <div className="flex min-h-[61px] items-center space-x-2 py-1">
+      <Skeleton className="h-5 flex-1" />
+      <Skeleton className="h-5 w-5 rounded-sm" />
+    </div>
+  );
+};

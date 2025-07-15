cd308b5070cefbee465c3cd800deb953de08bdf9
ehh99 <jinung5@gmail.com>
2025-07-15 19:34:11 +0900

    refactor: queryClient 수정
    - 기존 queryClient가 컴포넌트 외부에 생성되어 있는 문제를 발견, 이는 use client 컴포넌트에서 이렇게 하면 queryClient 가 모든 사용자 요청간에 공유가됨.
    서버 렌더링 시 한 사용자의 데이터가 다른 사용자에게 노출될 수 있으며, Hydration 오류를 일으키는 원인이 되기 때문에 useState를 사용해 컴포넌트 내부에서 생성하도록 수정.

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -1,17 +1,28 @@
 "use client";
 
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "sonner";
-
-const queryClient = new QueryClient();
 
 interface ProvidersProps {
   children: ReactNode;
 }
 
 export const Providers = ({ children }: ProvidersProps) => {
+  // useState를 사용해 queryClient 인스턴스를 생성
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60 * 5,
+            refetchOnWindowFocus: false,
+          },
+        },
+      }),
+  );
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}

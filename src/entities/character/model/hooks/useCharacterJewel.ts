47a35fda6517b3d2f081db26da12d20a1684abb8
ehh99 <jinung5@gmail.com>
2025-08-30 21:30:17 +0900

    bugfix: 쥬얼 데이터 요청시 레벨 체크 추가

--- src/entities/character/model/hooks/useCharacterJewel.ts
+++ src/entities/character/model/hooks/useCharacterJewel.ts
@@ -3,11 +3,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { getCharacterJewel } from "@/entities/character/api/get-jewel";
 
-export const useCharacterJewel = (ocid: string | null) => {
+export const useCharacterJewel = (ocid: string | null, level: number) => {
   return useQuery({
     queryKey: ["characterJewel", ocid],
     queryFn: () => getCharacterJewel(ocid!),
-    enabled: !!ocid,
+    enabled: !!ocid && level >= 120,
     staleTime: 10 * 60 * 1000,
     gcTime: 15 * 60 * 1000,
   });

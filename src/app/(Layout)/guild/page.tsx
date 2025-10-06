1a4c1a35dcd82fb9e605bf66037415cc2be11480
ehh99 <jinung5@gmail.com>
2025-10-06 20:34:11 +0900

    feat(guild): 길드 검색 페이지 SEO 최적화
    - 기존 부족한 SEO 보강을 위해 Metadata(OG Image, Canonical) 보강 및 data-nosnippet, sr-only 태그 적용
    - data-nosnippet을 사용하여 기능적인 안내 멘트는 검색 결과에 차지하지 않게 하고 sr-only를 활용해 페이지의 목적 설명을 추가

--- src/app/(Layout)/guild/page.tsx
+++ src/app/(Layout)/guild/page.tsx
@@ -5,16 +5,32 @@ export const metadata: Metadata = {
   title: "메이플스토리M 길드 검색",
   description:
     "메엠지지 메이플스토리M 길드 검색을 통해 상세한 길드 정보를 확인해보세요.",
-  openGraph: {
-    title: "메이플스토리M 길드 검색 | 메엠지지",
-    description:
-      "메엠지지 메이플스토리M 길드 검색을 통해 상세한 길드 정보를 확인해보세요.",
-    url: "/guild",
+
+  alternates: {
+    canonical: "/guild",
   },
-  twitter: {
-    title: "메이플스토리M 길드 검색 | 메엠지지",
+
+  openGraph: {
+    title: "메이플스토리M 길드 검색 - 메엠지지",
     description:
-      "메엠지지 메이플스토리M 길드 검색을 통해 상세한 길드 정보를 확인해보세요.",
+      "메엠지지에서 메이플스토리M 길드 검색을 통해 상세한 길드 정보를 확인해보세요.",
+    url: "/guild",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "메엠지지 길드 검색",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "메이플스토리M 길드 검색 - 메엠지지",
+    description:
+      "메엠지지에서 메이플스토리M 길드 검색을 통해 상세한 길드 정보를 확인해보세요.",
+    images: ["/og-image.png"],
   },
 };
 
@@ -22,11 +38,22 @@ export default async function GuildPage() {
   return (
     <div className="flex flex-col items-center text-center">
       <h1 className="my-2 text-2xl font-bold md:text-3xl">길드 검색</h1>
-      <p className="text-muted-foreground mb-4 text-sm md:text-base">
+      <p
+        className="text-muted-foreground mb-4 text-sm md:text-base"
+        data-nosnippet
+      >
         메이플스토리M의 길드 데이터는 평균 15분 후 확인 가능합니다.
       </p>
+      <div className="sr-only">
+        <h2>메이플스토리M 길드 정보 조회 서비스</h2>
+        <p>
+          메엠지지에서 스카니아, 루나, 엘리시움 등 전 서버의 길드 정보를
+          검색하세요. 해당 길드의 정보와 길드원, 길드 스킬 및 어빌리티 정보를
+          상세하게 확인할 수 있습니다.
+        </p>
+      </div>
 
-      <div className="w-full max-w-lg min-w-[320px]">
+      <div className="w-full max-w-3xl">
         <GuildSearch />
       </div>
     </div>

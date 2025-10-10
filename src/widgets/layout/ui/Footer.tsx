556e9461f3b5b659c6c4ce47f6276f55fa10accb
ehh99 <jinung5@gmail.com>
2025-10-10 15:27:41 +0900

    style: Header, Footer 스타일 수정

--- src/widgets/layout/ui/Footer.tsx
+++ src/widgets/layout/ui/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="text-foreground bg-background flex items-center justify-center border-t py-4 text-center">
+    <footer className="text-foreground bg-muted/70 flex items-center justify-center border-t py-4 text-center">
       <p className="text-xs antialiased">
         © 2025 <span className="font-semibold">메엠지지.</span>
         <br />
--- src/widgets/layout/ui/header/Header.tsx
+++ src/widgets/layout/ui/header/Header.tsx
@@ -7,9 +7,8 @@ import { HeaderMobileNav } from "./HeaderMobileNav";
 
 export function Header() {
   return (
-    <header className="bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full backdrop-blur">
-      {/* ✅ [수정됨] mx-auto 추가: 컨테이너 자체를 화면 중앙에 정렬 */}
-      <div className="flex h-16 items-center px-4 md:h-20 md:px-8">
+    <div className="container-wrapper wide:px-0 px-4">
+      <div className="flex h-16 items-center md:h-20">
         {/* 좌측: 로고 + 데스크탑 네비게이션 */}
         <div className="mr-4 flex items-center md:mr-12">
           <HeaderLogo />
@@ -22,6 +21,6 @@ export function Header() {
           <HeaderMobileNav />
         </div>
       </div>
-    </header>
+    </div>
   );
 }

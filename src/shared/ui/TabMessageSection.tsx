67acaadd9318d748ed4680ca119f3131575b1f56
ehh99 <jinung5@gmail.com>
2025-11-22 16:24:04 +0900

    style: 탭 내 공용 메시지 컴포넌트 높이 최대로 설정

--- src/shared/ui/TabMessageSection.tsx
+++ src/shared/ui/TabMessageSection.tsx
@@ -15,7 +15,7 @@ export const TabMessageSection = ({
   return (
     <section
       className={cn(
-        "bg-card flex min-h-91.5 flex-col items-center justify-center gap-2 p-6 text-center",
+        "bg-card flex h-full min-h-91.5 flex-col items-center justify-center gap-2 p-6 text-center",
         className,
       )}
     >
